ebbbd4c93110a8d2f64c96f56f1ea3037a73a646
Agnieszka Zagorna <agza@itu.dk>
2020-11-21 01:05:39 +0100

    added working report list and alert, prototyped database

--- ScooterApp/ReportsScreen/ReportsList.swift
+++ ScooterApp/ReportsScreen/ReportsList.swift
@@ -2,15 +2,23 @@ import SwiftUI
 
 struct ReportsList: View {
 
-    let reports = ["Report1", "Report2", "Report3", "..."]
+    //let reports = ["Report1", "Report2", "Report3", "..."]
+    
+//TODO: change UUID to something readable, but what? Date?
+//TODO: maybe fix the double navigation bar
 
-    var body: some View {
-            List(reports, id: \.self) { report in
-                Label(report, systemImage: "photo")
-            }.navigationBarTitle("My Reports", displayMode: .inline)
+    var body: some View
+    {
+        NavigationView {
+                List(ReportStore.shared.reports, id: \.self) { report in
+                    NavigationLink(destination: ReportView().environmentObject(report)) {
+                                    Label(report.id.uuidString, systemImage: "photo")
+                    }
+                }.navigationBarTitle("My reports", displayMode: .inline)
+        }
     }
 }
-
+    
 struct ReportsScreenView_Previews: PreviewProvider {
     static var previews: some View {
         ReportsList()
